dfab7c69891c70c5da6b1d821c5be116a9cda226
Alyona2209 <alyonka1991@gmail.com>
2019-08-10 00:09:59 +0300

    Renamed working files. Make list of test-methods. Start work with button-compponent.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,20 +5,20 @@
 describe('my app', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /test_button when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/test_button");
   });
 
 
-  describe('view1', function() {
+  describe('test_button', function() {
 
     beforeEach(function() {
-      browser.get('index.html#!/view1');
+      browser.get('index.html#!/test_button');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render test_button when user navigates to /test_button', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
